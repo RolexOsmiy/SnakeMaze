1e5596404676662d25c34dbdd77536fa7011f49b
RolexOsmiy <wow.rolex@yandex.by>
2016-08-17 17:48:13 +0300

    first commit

---
+++ Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	public GameObject bullet;
+	public Transform spawnBullet;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Mouse0))
+			{
+			Instantiate (bullet, spawnBullet.position, spawnBullet.rotation);
+			}
+		
+	}
+}
