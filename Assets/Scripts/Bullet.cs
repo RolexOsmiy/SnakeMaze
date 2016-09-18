8757fb49cbf85f8ec5412ce45fa258f30e4c91a7
RolexOsmiy <wow.rolex@yandex.by>
2016-09-18 12:55:42 +0300

    12321

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Bullet : NetworkBehaviour {
+	private int damage = 25;
+	private Collider hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		if(collision.transform.tag == "Player" && !isLocalPlayer)
+		{
+			string uIdentity = collision.transform.name;
+			CmdTellServerWhoWasShot(uIdentity, damage);
+		}
+
+		else if(collision.transform.tag == "Zombie")
+		{
+			string uIdentity = collision.transform.name;
+			CmdTellServerWhichZombieWasShot(uIdentity, damage);
+		}
+	}
+
+	[Command]
+	void CmdTellServerWhoWasShot (string uniqueID, int dmg)
+	{
+		GameObject go = GameObject.Find(uniqueID);
+		go.GetComponent<Player_Health>().DeductHealth(dmg);
+	}
+
+	[Command]
+	void CmdTellServerWhichZombieWasShot (string uniqueID, int dmg)
+	{
+		GameObject go = GameObject.Find(uniqueID);
+		go.GetComponent<Zombie_Health>().DeductHealth(dmg);
+	}
+}
---
+++ Assets/Scripts/CurrentGun.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrentGun : MonoBehaviour {
+
+	public GameObject Weapon01;
+	public GameObject Weapon02;
+	public GameObject Weapon03;
+	public GameObject Weapon04;
+	public GameObject Weapon05;
+	public GameObject Weapon06;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Alpha1))
+			{
+				Weapon01.SetActive(true);
+				Weapon02.SetActive(false);
+				Weapon03.SetActive(false);
+				Weapon04.SetActive(false);
+				Weapon05.SetActive(false);
+				Weapon06.SetActive(false);
+			}
+		if (Input.GetKey(KeyCode.Alpha2))
+				{
+					Weapon01.SetActive(false);
+					Weapon02.SetActive(true);
+					Weapon03.SetActive(false);
+					Weapon04.SetActive(false);
+					Weapon05.SetActive(false);
+					Weapon06.SetActive(false);
+				}
+		if (Input.GetKey(KeyCode.Alpha3))
+					{
+						Weapon01.SetActive(false);
+						Weapon02.SetActive(false);
+						Weapon03.SetActive(true);
+						Weapon04.SetActive(false);
+						Weapon05.SetActive(false);
+						Weapon06.SetActive(false);
+					}
+		if (Input.GetKey(KeyCode.Alpha4))
+						{
+							Weapon01.SetActive(false);
+							Weapon02.SetActive(false);
+							Weapon03.SetActive(false);
+							Weapon04.SetActive(true);
+							Weapon05.SetActive(false);
+							Weapon06.SetActive(false);
+						}
+		if (Input.GetKey(KeyCode.Alpha5))
+							{
+								Weapon01.SetActive(false);
+								Weapon02.SetActive(false);
+								Weapon03.SetActive(false);
+								Weapon04.SetActive(false);
+								Weapon05.SetActive(true);
+								Weapon06.SetActive(false);
+							}
+		if (Input.GetKey(KeyCode.Alpha6))
+								{
+									Weapon01.SetActive(false);
+									Weapon02.SetActive(false);
+									Weapon03.SetActive(false);
+									Weapon04.SetActive(false);
+									Weapon05.SetActive(false);
+									Weapon06.SetActive(true);
+								}
+	}
+}
